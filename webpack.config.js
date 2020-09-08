bc81421f4a8bf833877a06e71fe1279241582185
Yashanshu Bhardwaj <yashansh3b@gmail.com>
2020-09-08 19:13:06 +0530

    setting up

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/app.js",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: [
+              ["@babel/plugin-proposal-class-properties", { loose: true }],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", 'sass-loader'],
+      },
+    ],
+  },
+  devtool: "eval-cheap-module-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+  },
+};
