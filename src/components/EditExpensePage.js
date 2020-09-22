c8a833d065ed22cbad295636930ca130fb0ba28b
Yashanshu Bhardwaj <yashansh3b@gmail.com>
2020-09-22 19:03:46 +0530

    functionality to add and edit.

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { connect } from "react-redux";
+import ExpenseForm from "./ExpenseForm";
+import { editExpense } from "../actions/expenses";
+
 const EditExpensePage = (props) => {
-  return <div>Editing the expense with id of {props.match.params.id}</div>;
+  return (
+    <div>
+      Editing the expense with id of {props.match.params.id}
+      <ExpenseForm
+        expense={props.expense}
+        onSubmit={(expense) => {
+          props.dispatch(editExpense(props.expense.id, expense));
+          props.history.push("/");
+        }}
+      />
+    </div>
+  );
 };
-export default EditExpensePage;
+
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find(
+      (expense) => expense.id === props.match.params.id
+    ),
+  };
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,11 +1,25 @@
 import React from "react";
+import moment from "moment";
+import "react-dates/initialize";
+import { SingleDatePicker } from "react-dates";
+import "react-dates/lib/css/_datepicker.css";
+
+const now = moment();
+console.log(now.format("MMM Do, YYYY"));
 
 export default class ExpenseForm extends React.Component {
-  state = {
-    description: "",
-    note: "",
-    amount: "",
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      description: props.expense ? props.expense.description : "",
+      note:props.expense ? props.expense.note : "" ,
+      amount: props.expense ? (props.expense.amount / 100) : "" ,
+      createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+      calenderFocused: false,
+      error: "",
+    };
+  }
   onDescriptionChange = (e) => {
     const description = e.target.value;
     this.setState(() => ({
@@ -20,14 +34,31 @@ export default class ExpenseForm extends React.Component {
   };
   onAmountChange = (e) => {
     const amount = e.target.value;
-    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }));
+    }
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!this.state.description || !this.state.amount) {
+      const error = "Please provide a description and amount";
+      this.setState(() => ({ error }));
+    } else {
+      this.setState(() => ({ error: "" }));
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note,
+      });
     }
   };
   render() {
     return (
       <div>
-        <form>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.onSubmit}>
           <input
             type="text"
             placeholder="Description"
@@ -35,17 +66,36 @@ export default class ExpenseForm extends React.Component {
             value={this.state.description}
             onChange={this.onDescriptionChange}
           />
+
           <input
             type="text"
             placeholder="Amount"
             value={this.state.amount}
             onChange={this.onAmountChange}
           />
+
           <textarea
             placeholder="Add a note for your expense (optional)"
             value={this.state.note}
             onChange={this.onNoteChange}
           ></textarea>
+
+          <SingleDatePicker
+            date={this.state.createdAt} // momentPropTypes.momentObj or null
+            onDateChange={(createdAt) => {
+              if (createdAt) {
+                this.setState({ createdAt });
+              }
+            }} // PropTypes.func.isRequired
+            focused={this.state.calenderFocused} // PropTypes.bool
+            onFocusChange={({ focused }) =>
+              this.setState({ calenderFocused: focused })
+            } // PropTypes.func.isRequired
+            numberOfMonths={1}
+            isOutsideRange={() => false}
+            id="datat" // PropTypes.string.isRequired,
+          />
+
           <button>Add Expense</button>
         </form>
       </div>
